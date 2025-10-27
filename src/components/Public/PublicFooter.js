12d2edd489d277149269e3ab096c0ccd7dfa52af
Jeremiah Sheehy <jameys127@gmail.com>
2025-10-26 23:32:54 -0700

    1.0.1 added a favicon and fixed the resume

--- src/components/Public/PublicFooter.js
+++ src/components/Public/PublicFooter.js
@@ -24,7 +24,7 @@ const PublicFooter = () => {
           </a>
         </li>
         <li>
-          <a href="/JeremiahSheehyResumeEdit.pdf" target="_blank" rel="noopener noreferrer">
+          <a href="JeremiahSheehyResumeEdit.pdf" target="_blank" rel="noopener noreferrer">
             Resume <i className="fa-solid fa-file"></i>
           </a>
         </li>
